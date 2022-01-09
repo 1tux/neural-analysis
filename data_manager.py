58fb3ac4ab0537bd04e5cc474d27356d69072e76
1tux <itayona@gmail.com>
2022-01-09 11:29:46 +0200

    support different neurons

--- data_manager.py
+++ data_manager.py
@@ -8,6 +8,20 @@ from dataclasses import dataclass
 from conf import Conf
 import features_lib
 
+def neuron_id_to_day(neuron_id):
+    if 1 <= neuron_id <= 29: return "d191222"
+    if 31 <= neuron_id <= 53: return "d191221"
+    if 56 <= neuron_id <= 80: return "d191220"
+    if 82 <= neuron_id <= 104: return "d191223"
+    if 106 <= neuron_id <= 129: return "d191224"
+    if 132 <= neuron_id <= 144: return "d191225"
+    if 145 <= neuron_id <= 161: return "d191226"
+    if 163 <= neuron_id <= 174: return "d191229"
+    if 176 <= neuron_id <= 190: return "d191231"
+    if 191 <= neuron_id <= 207: return "d200101"
+    if 208 <= neuron_id <= 216: return "d200102"
+    if 217 <= neuron_id <= 227: return "d200108"
+
 class DataProp:
     '''
         Data + Properties that were captured on the data.
@@ -74,8 +88,10 @@ class Loader1(DataLoader):
 
 class Loader2(DataLoader):
     def __call__(self, nid: int) -> pd.DataFrame:
-        df = pd.read_csv(r"C:\Users\root34\Documents\university\MSc\Bat Lab\git\neural-analysis\inputs\b2305_d191220_simplified_behaviour.csv").drop(columns=['Unnamed: 0'])
-        neuron_path = r"C:\Users\root34\Documents\university\MSc\Bat Lab\git\neural-analysis\inputs\72_b2305_d191220_cell_analysis.mat"
+        day = neuron_id_to_day(nid)
+        df = pd.read_csv(fr"C:\Users\root34\Documents\university\MSc\Bat Lab\git\neural-analysis\inputs\b2305_{day}_simplified_behaviour.csv").drop(columns=['Unnamed: 0'])
+        # neuron_path = fr"C:\Users\root34\Documents\university\MSc\Bat Lab\git\neural-analysis\inputs\{nid}_b2305_{day}_cell_analysis.mat"
+        neuron_path = fr"C:\tmp\20210506 - neurons\{nid}_b2305_{day}_cell_analysis.mat"
         d = h5py.File(neuron_path, "r")
         spikes = np.array(d['cell_analysis']['spikes_per_frame']).T[0]
         df['neuron'] = spikes
